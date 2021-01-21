5076fb20ae05e7db62e415cb2f601ca52c951c5d
ebrahimdev01 <ebrahimdev01@gmail.com>
2021-01-21 13:30:34 +0330

    non problem

--- PleerMusic/PleerMusic.App/Form1.cs
+++ PleerMusic/PleerMusic.App/Form1.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace PleerMusic.App
 {
-    public partial class Form1 : Form
+    public partial class PleerMusic : Form
     {
-        public Form1()
+        public PleerMusic()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
