43c79ff567bcdc56d0595b8dc58bc3d4c86b5d13
ebrahimdev01 <ebrahimdev01@gmail.com>
2021-01-21 16:41:17 +0330

    add and ToComplete UnitofWork , PleerMusicGenelear

---
+++ PleerMusic/PleerMusic.DataLayer/Context/uowPleerMusic.cs
@@ -0,0 +1,80 @@
+﻿using PleerMusic.DataLayer.Models;
+
+using PleerMusic.DataLayer.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PleerMusic.DataLayer.Context
+{
+    public class uowPleerMusic : IDisposable
+    {
+
+        private PleerMusicDB Db;
+
+        public uowPleerMusic()
+        {
+            Db = new PleerMusicDB();
+        }
+
+
+        private PleerMusicGeneric<Music> _music;
+
+        public PleerMusicGeneric<Music> pMusicGeneric
+        {
+            get
+            {
+                if (_music == null)
+                {
+                    _music = new PleerMusicGeneric<Music>(Db);
+                }
+                return _music;
+            }
+        }
+
+
+        private PleerMusicGeneric<AlbumMusic> _albummusic;
+
+        public PleerMusicGeneric<AlbumMusic> pAlbumMusic
+        {
+            get
+            {
+                if (_albummusic == null)
+                {
+                    _albummusic = new PleerMusicGeneric<AlbumMusic>(Db);
+                }
+                return _albummusic;
+            }
+        }
+
+
+
+
+        private PleerMusicGeneric<Singer> _singer;
+
+        public PleerMusicGeneric<Singer> pSinger
+        {
+            get
+            {
+                if (_singer == null)
+                {
+                    _singer = new PleerMusicGeneric<Singer>(Db);
+                }
+                return _singer;
+            }
+        }
+
+
+        public void Save()
+        {
+            Db.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            Db?.Dispose();
+        }
+    }
+}
---
+++ PleerMusic/PleerMusic.DataLayer/Services/PleerMusicGeneric.cs
@@ -0,0 +1,80 @@
+﻿using PleerMusic.DataLayer.Context;
+
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PleerMusic.DataLayer.Services
+{
+    public class PleerMusicGeneric<TEntity> where TEntity : class
+    {
+
+        private PleerMusicDB _Db;
+        private DbSet<TEntity> _DbSet;
+
+        public PleerMusicGeneric(PleerMusicDB Db)
+        {
+            _Db = Db;
+            _DbSet = Db.Set<TEntity>();
+        }
+
+        IEnumerable<TEntity> GetAll(Expression<Func<TEntity, bool>> where = null)
+        {
+
+            IQueryable<TEntity> query = _DbSet;
+
+            if (where != null)
+            {
+                query = query.Where(where);
+            }
+
+            return query.ToList();
+
+        }
+
+        TEntity GetById(object ID)
+        {
+            return _DbSet.Find(ID);
+        }
+
+
+        void Insert(TEntity entity)
+        {
+
+            attaching(entity);
+
+            _Db.Entry(entity).State = EntityState.Added;
+
+        }
+
+        void Delete(TEntity entity)
+        {
+            attaching(entity);
+
+            _Db.Entry(entity).State = EntityState.Deleted;
+        }
+
+        void Delete(object ID)
+        {
+
+            TEntity res = GetById(ID);
+
+            Delete(res);
+
+        }
+        void attaching(TEntity entity)
+        {
+            if (_Db.Entry(entity).State == EntityState.Detached)
+            {
+                _DbSet.Attach(entity);
+            }
+
+        }
+    }
+
+
+}
