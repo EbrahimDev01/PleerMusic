383658f6070fc77b028f96a7467fb40ddec2be82
ebrahimdev01 <ebrahimdev01@gmail.com>
2021-01-22 14:49:03 +0330

    Add method mergNameSinger and Get Name Album and Singer with Include

--- PleerMusic/PleerMusic.App/Controls/ConvertMusicToControl.cs
+++ PleerMusic/PleerMusic.App/Controls/ConvertMusicToControl.cs
@@ -8,6 +8,7 @@ using System.Windows.Forms;
 using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using PleerMusic.DataLayer.Models;
 
 namespace PleerMusic.App
 {
@@ -29,20 +30,23 @@ namespace PleerMusic.App
                 using (uowPleerMusic db = new uowPleerMusic())
                 {
 
-                    
 
+                    
 
                     return db.pMusic.GetAll()
                         .Select(
                         m =>
                         new vmMusic
                         {
-                            AlbumThisMusic = (m.AlbumThisMusic == null) ? "" : m.AlbumThisMusic.NameAlmum,
+                            AlbumThisMusic = ((AlbumMusic)(db.pMusic.GetAll(m1 => m1.MusicID == m1.MusicID, "AlbumThisMusic"))?.FirstOrDefault().AlbumThisMusic).NameAlmum,
                             MusicAddress = m.MusicAddress,
                             MusicName = m.MusicName,
-                            SingerThisMusic = "",
+                            SingerThisMusic = (mergeNameSinger((List<Singer>)(db.pMusic.GetAll(m1 => m1.MusicID == m1.MusicID, "SingerThisMusic")).FirstOrDefault().SingerThisMusic)).ToString(),
                             MusicID = m.MusicID
                         }).ToList();
+
+                    //((db.pMusic.GetAll(i => i.MusicID == m.MusicID, "SingerThisMusic")).ToArray())
+
 
 
                 }
@@ -52,39 +56,53 @@ namespace PleerMusic.App
         private async static Task<List<Control>> ucMusics(List<vmMusic> musics)
         {
             return await Task<List<Control>>.Run(() =>
+            {
+                List<Control> controls = new List<Control>();
+                Panel p1 = new Panel();
+                p1.Dock = DockStyle.Top;
+                p1.BackColor = System.Drawing.Color.Black;
+                p1.Height = 2;
+                p1.TabIndex = 2;
+                controls.Add(p1);
+                foreach (var m in musics)
                 {
-                    List<Control> controls = new List<Control>();
-                    Panel p1 = new Panel();
-                    p1.Dock = DockStyle.Top;
-                    p1.BackColor = System.Drawing.Color.Black;
-                    p1.Height = 2;
-                    p1.TabIndex = 2;
-                    controls.Add(p1);
-                    foreach (var m in musics)
-                    {
-                        var ucMusic = new ucMusicShow();
-                        ucMusic.lblNameMusic.Text = m.MusicName;
-                        ucMusic.lblNameSinger.Text = m.SingerThisMusic;
-                        ucMusic.Dock = DockStyle.Top;
-                        controls.Add(ucMusic);
-                        ucMusic.TabIndex = 1;
+                    var ucMusic = new ucMusicShow();
+                    ucMusic.lblNameMusic.Text = m.MusicName;
+                    ucMusic.lblNameSinger.Text = m.SingerThisMusic;
+                    ucMusic.Dock = DockStyle.Top;
+                    controls.Add(ucMusic);
+                    ucMusic.TabIndex = 1;
 
-                        Panel p = new Panel();
-                        p.Dock = DockStyle.Top;
-                        p.BackColor = System.Drawing.Color.Black;
-                        p.Height = 2;
-                        p.TabIndex = 2;
-                        controls.Add(p);
+                    Panel p = new Panel();
+                    p.Dock = DockStyle.Top;
+                    p.BackColor = System.Drawing.Color.Black;
+                    p.Height = 2;
+                    p.TabIndex = 2;
+                    controls.Add(p);
 
 
-                    }
+                }
 
-                    return controls;
-                });
+                return controls;
+            });
+        }
+
+        private static string mergeNameSinger(List<Singer> nameSinger)
+        {
+            string names = "";
+
+            foreach (Singer singer in nameSinger)
+            {
+                names += singer.SignerName + " - ";
+            }
+            names = names.Remove(names.Length - 3);
+            return names;
+
+
         }
 
 
-        
 
     }
 }
+
