70fbb70bac11f2d3afb7c007a962fb0d8c1a23b3
ebrahimdev01 <ebrahimdev01@gmail.com>
2021-01-23 12:20:39 +0330

    Install-Package taglib -Version 2.1.0 and Create Class ConvertAddresTOClass

---
+++ PleerMusic/PleerMusic.Utility/MusicControl/ConvertAddresToClass.cs
@@ -0,0 +1,18 @@
+﻿using PleerMusic.DataLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Id3Lib.Exceptions;
+
+using System.Windows.Forms;
+
+namespace PleerMusic.Utility.MusicControl
+{
+    public class ConvertAddresToClass
+    {
+
+        
+    }
+}
