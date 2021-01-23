44afccf03294fe4ea77fda49aca0171999075152
ebrahimdev01 <ebrahimdev01@gmail.com>
2021-01-23 15:05:51 +0330

    Install Install-Package WMPLib -Version 1.0.0 and NAudio For play music And Create Class PlayMusicControl

---
+++ PleerMusic/PleerMusic.Utility/MusicControl/PlayMusic/PlayMusicContorl.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NAudio.Wave;
+using WMPLib;
+
+namespace PleerMusic.App.Controls.PlayMusic
+{
+    public static class PlayMusicContorl
+    {
+        //private void ss(string address = "")
+        //{
+        //    AudioFileReader q =new AudioFileReader(address);
+        //}
+    }
+}
